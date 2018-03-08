ed57126986186f105fe0bccd61bbe685936be4cd
jarrodyellets <jarrod524@gmail.com>
2018-03-08 22:47:16 +0100

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+	$(window).scroll(function() {
+		$(".title").css("opacity", 1 - $(window).scrollTop() / 250);
+	  if ($(document).scrollTop() > 50) {
+	    $('.navbar-brand').addClass('shrinkBrand');
+	    $('.link').addClass('shrinkMenu');
+	    $('.navbar').addClass('darkBackground');
+
+	  } else {
+	    $('.navbar-brand').removeClass('shrinkBrand');
+	    $('.link').removeClass('shrinkMenu');
+	    $('.navbar').removeClass('darkBackground');
+	  }
+	});
+
+	$(".navbar-toggle").click(function(){
+		if ($(document).scrollTop() < 50) {
+			$(".navbar").toggleClass("darkBackground");
+		}
+	})
+
+	$('#n-home').click(function(){
+			scroll_to(".contain");
+	})
+	$('#n-about').click(function(){
+      scroll_to(".about");
+    });
+	$('#n-menu').click(function(){
+      scroll_to(".menu");
+    });
+	$('#n-contact').click(function(){
+      scroll_to(".contact")
+    });
+	$('#n-press').click(function(){
+      scroll_to(".press")
+    });
+
+	function scroll_to(div){
+	  $('html, body').animate({
+	    scrollTop: $(div).offset().top
+	    }, 500);
+}
+
+})
