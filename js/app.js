e865f1168d19bc57218301a5c2ef73e268d4660e
jarrodyellets <jarrod524@gmail.com>
2018-08-29 08:46:30 +0200

    Beautify

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-//Nav Bar animation
-  $(window).scroll(function() {
+  //Nav Bar animation
+  $(window).scroll(function () {
     $(".title").css("opacity", 1 - $(window).scrollTop() / 250);
     if ($(document).scrollTop() > 50) {
       $('.navbar-brand').addClass('shrinkBrand');
@@ -14,33 +14,33 @@ $(document).ready(function(){
     }
   });
 
-  $(".navbar-toggle").click(function(){
+  $(".navbar-toggle").click(function () {
     if ($(document).scrollTop() < 50) {
       $(".navbar").toggleClass("darkBackground");
     }
   })
 
-//Auto Scroll
-  $('#n-home').click(function(){
-      scroll_to(".contain");
+  //Auto Scroll
+  $('#n-home').click(function () {
+    scroll_to(".contain");
   })
-  $('#n-about').click(function(){
-      scroll_to(".about");
-    });
-  $('#n-menu').click(function(){
-      scroll_to(".menu");
-    });
-  $('#n-contact').click(function(){
-      scroll_to(".contact")
-    });
-  $('#n-press').click(function(){
-      scroll_to(".press")
-    });
+  $('#n-about').click(function () {
+    scroll_to(".about");
+  });
+  $('#n-menu').click(function () {
+    scroll_to(".menu");
+  });
+  $('#n-contact').click(function () {
+    scroll_to(".contact")
+  });
+  $('#n-press').click(function () {
+    scroll_to(".press")
+  });
 
-  function scroll_to(div){
+  function scroll_to(div) {
     $('html, body').animate({
       scrollTop: $(div).offset().top
-      }, 500);
-}
+    }, 500);
+  }
 
 })
